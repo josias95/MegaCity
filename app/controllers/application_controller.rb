4eea639cbbd0a5babd0ca89cda93716b1df1310f
josias95 <jcubillosg@uqvirtual.edu.co>
2017-11-02 17:21:28 -0500

    Correccion error regitrar usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
 	def authenticate_admin!
 		redirect_to root_path unless usuario_signed_in? && current_usuario.is_admin?
 	end
+
+	def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :email, :password, :apellidos, :telefono, :ciudad, :direccion, :fechaNacimmiento, :genero])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :email, :password, :current_password, :apellidos, :telefono, :ciudad, :direccion, :fechaNacimmiento, :genero])
+    end
 end
