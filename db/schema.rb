06ca9f2481175cb6b7fa960e5bf86da58106481f
josias95 <jcubillosg@uqvirtual.edu.co>
2017-11-02 15:54:17 -0500

    Se agrego puntos a usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102193231) do
+ActiveRecord::Schema.define(version: 20171102203247) do
 
   create_table "almacens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "nombre"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20171102193231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "permission_level", default: 1
+    t.integer "puntos", default: 0
     t.index ["email"], name: "index_usuarios_on_email", unique: true
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
